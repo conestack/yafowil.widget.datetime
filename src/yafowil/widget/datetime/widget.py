fadd057b32104ecd36cd473ccb34f02ec063a5ff
rnix <rnix@f119a143-da5b-4457-81e3-bee00661a780>
2010-05-08 10:17:16 +0000

    git-svn-id: https://svn.bluedynamics.eu/svn/module/yafowil.widget.datetime/trunk@2207 f119a143-da5b-4457-81e3-bee00661a780

--- src/yafowil/widget/datetime/widget.py
+++ src/yafowil/widget/datetime/widget.py
@@ -4,34 +4,42 @@ from yafowil.base import (
     ExtractionError,
 )
 from yafowil.common import (
+    _value,
     generic_extractor,
     generic_required_extractor,
-    input_generic_renderer,
-    InputGenericPreprocessor,
 )
 from yafowil.utils import (
     tag,
+    cssid,
     cssclasses,
+)
+from bda.intellidatetime import (
+    convert,
+    DateTimeConversionError,
 )
 
 def datetime_extractor(widget, data):
-    import pdb;pdb.set_trace()
+    try:
+        # XXX not only 'iso' date.
+        return convert(data.extracted, time=None, tzinfo=None, locale='iso')
+    except DateTimeConversionError:
+        raise ExtractionError('Not a valid date input.')
 
 def datetime_renderer(widget, data):
-    attrs = {
-        'src': widget.attrs.icon,
-        'alt': widget.attrs.alt,
-        'title': widget.attrs.title,
-        'class_': cssclasses(widget, data, widget.attrs.class_)
+    classes = list()
+    if widget.attrs.get('datepicker'):
+        classes.append('datepicker')
+    input_attrs = {
+        'type': 'text',
+        'value':  _value(widget, data),
+        'name_': widget.dottedpath,
+        'id': cssid(widget, 'input'),    
+        'class_': cssclasses(widget, data, *classes),    
     }
-    return data.rendered + tag('img', **attrs)
+    return tag('input', **input_attrs)
 
 factory.defaults['datetime.required_class'] = 'required'
 factory.defaults['datetime.default'] = ''
-factory.defaults['datetime.icon'] = '/calendar16_16.png'
-factory.defaults['datetime.alt'] = 'Calendar'
-factory.defaults['datetime.title'] = 'Choose Date'
-factory.defaults['datetime.class_'] = 'triggerdatepicker'
 factory.register(
     'datetime', 
     [
@@ -40,10 +48,6 @@ factory.register(
         datetime_extractor,
     ], 
     [
-        input_generic_renderer,
         datetime_renderer,
-    ],
-    [
-        InputGenericPreprocessor('text'),
     ],
 )
