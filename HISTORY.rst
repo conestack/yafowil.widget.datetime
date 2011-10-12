a4f11c0e6bcbb78f83795b51abf54c0ce2cc455f
Robert Niederreiter <rnix@squarewave.at>
2011-10-12 09:53:43 +0200

    adopt to latest array changes

--- HISTORY.rst
+++ HISTORY.rst
@@ -5,7 +5,7 @@ History
 1.1.1dev
 --------
 
-- add datepicker binder function to ``yafowil.widget.array`` binders if found
+- add datepicker binder function to ``yafowil.widget.array`` hooks if found
   [rnix, 2011-10-05]
 
 1.1
--- src/yafowil/widget/datetime/resources/widget.js
+++ src/yafowil/widget/datetime/resources/widget.js
@@ -20,9 +20,9 @@ if (typeof(window['yafowil']) == "undefined") yafowil = {};
             });
         }
         
-        // add binder to yafowil.widget.array binders
+        // add binder to yafowil.widget.array hooks
         if (typeof(window.yafowil['array']) != "undefined") {
-            $.extend(yafowil.array.binders, {
+            $.extend(yafowil.array.hooks.add, {
                 datepicker_binder: yafowil.datepicker.binder
             });
         }
