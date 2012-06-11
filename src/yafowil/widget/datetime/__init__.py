7cdbede0e5a5e908a0a513e399f597c5bbd386ed
Robert Niederreiter <office@squarewave.at>
2012-06-11 23:48:33 +0200

    change resource providing

--- src/yafowil/widget/datetime/__init__.py
+++ src/yafowil/widget/datetime/__init__.py
@@ -1,20 +1,29 @@
 import os 
 
+
 def register():
     import widget
-    
+
+
 def get_resource_dir():
     return os.path.join(os.path.dirname(__file__), 'resources')
 
-def get_js(thirdparty=True):
-    js = list()
-    if thirdparty:
-        js.append('jquery-ui-1.8.18.datepicker.min.js')
-    js.append('widget.js')
-    return js
 
-def get_css(thirdparty=True):
-    css = list()
-    if thirdparty:
-        css.append('jquery-ui-1.8.18.datepicker.css')
-    return css
+def get_js():
+    return [{
+        'resource': 'jquery-ui-1.8.18.datepicker.min.js',
+        'thirdparty': True,
+        'order': 20,
+    }, {
+        'resource': 'widget.js',
+        'thirdparty': False,
+        'order': 21,
+    }]
+
+
+def get_css():
+    return [{
+        'resource': 'jquery-ui-1.8.18.datepicker.css',
+        'thirdparty': True,
+        'order': 20,
+    }]
