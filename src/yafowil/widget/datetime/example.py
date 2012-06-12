b0fd64965ebaa4f9f164fba64c651dfcede870a1
Jens W. Klein <jens@bluedynamics.com>
2012-06-13 00:20:28 +0200

    refactored examples

--- src/yafowil/widget/datetime/example.py
+++ src/yafowil/widget/datetime/example.py
@@ -1,17 +1,18 @@
 from yafowil.base import factory
 
-
 def get_example():
-    part = factory(u'fieldset', name='yafowilwidgetdatetime')
-    part['date'] = factory('field:label:error:datetime', props={
-        'label': 'Search or enter a datetime',
-        'required': True,
-        'locale': 'de', # XXX: iso -> deliver language code in dom for JS
-        'datepicker': True})
-    part['datetime'] = factory('field:label:error:datetime', props={
-        'label': 'Search or enter a datetime',
-        'required': True,
-        'locale': 'de', # XXX: iso -> deliver language code in dom for JS
-        'datepicker': True,
-        'time': True})
-    return {'widget': part, 'routes': {}}
+    part1 = factory(u'fieldset', name='yafowilwidgetdatetime.1')
+    part1['date'] = factory('field:label:error:datetime', props={
+         'label': 'Search or enter a datetime',
+         'required': True,
+         'locale': 'de', # XXX: iso -> deliver language code in dom for JS
+         'datepicker': True})
+    part2 = factory(u'fieldset', name='yafowilwidgetdatetime.2')
+    part2['datetime'] = factory('field:label:error:datetime', props={
+         'label': 'Search or enter a datetime',
+         'required': True,
+         'locale': 'de', # XXX: iso -> deliver language code in dom for JS
+         'datepicker': True,
+         'time': True})
+    return [{'widget': part1, 'doc': 'TODO 1'},
+            {'widget': part2, 'doc': 'TODO 2'}]
