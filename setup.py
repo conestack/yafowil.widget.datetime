56cc937746f4aeed6cda6b7db8e898d682226b4b
Jens W. Klein <jk@kleinundpartner.at>
2017-03-10 12:09:03 +0100

    Back to development: 1.10

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 import os
 
 
-version = '1.9'
+version = '1.10.dev0'
 shortdesc = 'Datetime Widget for YAFOWIL'
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'CHANGES.rst')).read()
