c2fb226e042235d7562c3cb39f38c6b93bc125f8
Robert Niederreiter <office@squarewave.at>
2020-07-09 13:45:33 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read_file(name):
         return f.read()
 
 
-version = '1.12'
+version = '1.13.dev0'
 shortdesc = 'Datetime Widget for YAFOWIL'
 longdesc = '\n\n'.join([read_file(name) for name in [
     'README.rst',
