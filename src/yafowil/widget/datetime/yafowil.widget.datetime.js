76088d1e908dd4566b12f920e82221e176a5428c
Lena Daxenbichler <lena.daxenbichler@r2solutions.at>
2021-08-16 11:03:59 +0200

    setup karma and rollup

---
+++ src/yafowil/widget/datetime/yafowil.widget.datetime.js
@@ -0,0 +1,15 @@
+(function () {
+	'use strict';
+
+
+
+
+	if (window.yafowil === undefined) {
+	    window.yafowil = {};
+	}
+
+	window.yafowil.datetime = exports;
+
+
+}());
+//# sourceMappingURL=yafowil.widget.datetime.js.map
