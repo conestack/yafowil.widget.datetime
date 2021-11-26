42aab4810b9600b7fb55bfc8c5ef54a9d3cdb662
Lena Daxenbichler <lena.daxenbichler@r2solutions.at>
2021-11-26 10:50:53 +0100

    add toggle button to Datepicker

--- js/src/datetime.js
+++ js/src/datetime.js
@@ -9,12 +9,31 @@ export class DateTimeWidget {
     static initialize(context) {
         $('input.datepicker', context).each(function() {
             let elem = $(this);
-            new Datepicker(elem[0], {
+            let picker = new Datepicker(elem[0], {
                 orientation: 'bottom',
                 buttonClass: 'bs4-btn',
                 weekStart: 1,
                 todayHighlight: true
             });
+            let btn_trigger = $(`<button>...</button>`)
+                .addClass('datepicker-trigger btn btn-default');
+            elem.after(btn_trigger);
+
+            let toggle_view = toggle_picker.bind(picker);
+
+            btn_trigger.off('mousedown', toggle_view).on('mousedown', toggle_view);
+            btn_trigger.on('click', (e) => {e.preventDefault();});
+
+            function toggle_picker(evt) {
+                evt.preventDefault();
+                evt.stopPropagation();
+
+                if ($(picker.picker.element).hasClass('active')) {
+                    picker.hide();
+                } else {
+                    picker.show();
+                }
+            }
         });
         $('input.timepicker', context).each(function() {
             let elem = $(this);
